04c3a783a2dc72ae235ab92201a2988a6e14279b
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-01-23 10:09:18 -0500

    seed file update

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Boss.destroy_all
+Tasker.destroy_all
+
+3.times do |index|
+  Boss.create!(name: Faker::Name.name,
+                email: Faker::Internet.email,
+                username: Faker::Internet.username(6),
+                city: Faker::Address.state,
+               )
+  end
+
+	3.times do |index|
+	  Tasker.create!(name: Faker::Name.name,
+	                email: Faker::Internet.email,
+	                username: Faker::Internet.username(6),
+	                city: Faker::Address.state,
+	                bio: Faker::Lorem.paragraph(4),
+	                vehicle: Faker::Boolean.boolean(0.8)
+	               )
+	  end
