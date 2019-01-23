1e49701a2a60071293033c98049505b543f4c157
aberm <abermendel@gmail.com>
2019-01-23 11:05:28 -0500

    updated validations to tasks

--- app/models/task.rb
+++ app/models/task.rb
@@ -3,10 +3,9 @@ class Task < ApplicationRecord
   belongs_to :tasker, optional: true
 
   validates :location, presence: true
-  validates :description, presence: true, length: { minimum: 50, maximum: 1000 }
+  validates :description, presence: true, length: { minimum: 5, maximum: 250 }
   validates :vehicle, inclusion: { in: [ true, false ] }
-  validates :rating, numericality: {greater_than: 0, less_than: 6}, allow_nil: true
-  validates :review, presence: true, allow_nil: true
+  validates :rating, numericality: {greater_than: 0, less_than: 6}
   validates :minutes, presence: true, numericality: true
   validates :price_rate, presence: true, numericality: true
   validates :completed, presence: true
