98ea23928c6db70d3bc035ac3fe1f6c20a6f746a
aberm <abermendel@gmail.com>
2019-01-23 11:32:59 -0500

    updated tasks views

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,12 +1,28 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @tasks = Task.all
+  end
+
   def new
     @task = Task.new
+    # @task.boss = session[:boss] ??
   end
 
   def create
-    # make validations
+    @task = Task.new(task_params)
+    @task.boss_id = task_params[:boss_id]
+    @task.minutes = task_params[:minutes].to_f * 60
+    @task.price_rate = task_params[:price_rate].to_f * 100
+
+    if @task.valid?
+      @task.save
+      redirect_to task_path(@task)
+     else
+      render :new
+      # make validations
+    end
   end
 
   def show
@@ -16,6 +32,18 @@ class TasksController < ApplicationController
   end
 
   def update
+    @task.update(task_params)
+    @task.boss_id = task_params[:boss_id]
+    @task.minutes = task_params[:minutes].to_f * 60
+    @task.price_rate = task_params[:price_rate].to_f * 100
+
+    if @task.valid?
+      @task.save
+      redirect_to task_path(@task)
+     else
+      render :edit
+      # make validations
+    end
   end
 
   def destroy
@@ -28,6 +56,6 @@ class TasksController < ApplicationController
   end
 
   def task_params
-    params.require(:task).permit(:description)
+    params.require(:task).permit(:boss_id, :location, :description, :category, :vehicle, :minutes, :price_rate)
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -5,9 +5,7 @@ class Task < ApplicationRecord
   validates :location, presence: true
   validates :description, presence: true, length: { minimum: 5, maximum: 250 }
   validates :vehicle, inclusion: { in: [ true, false ] }
-  validates :rating, numericality: {greater_than: 0, less_than: 6}
   validates :minutes, presence: true, numericality: true
   validates :price_rate, presence: true, numericality: true
-  validates :completed, presence: true
 
 end
