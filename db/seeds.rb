ea16ac5dc899f804ba08a35bb45cfea02dfab9de
aberm <abermendel@gmail.com>
2019-01-23 12:12:39 -0500

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,35 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
-
+Boss.destroy_all
+Tasker.destroy_all
 Task.destroy_all
+
+10.times do |index|
+  Boss.create!(name: Faker::Name.name,
+                email: Faker::Internet.email,
+                username: Faker::Internet.username(6),
+                city: Faker::Address.state
+               )
+  end
+#
+	10.times do |index|
+	  Tasker.create!(name: Faker::Name.name,
+	                email: Faker::Internet.email,
+	                username: Faker::Internet.username(6),
+	                city: Faker::Address.state,
+	                bio: Faker::Lorem.paragraph(4),
+	                vehicle: Faker::Boolean.boolean(0.8)
+	               )
+	  end
+	20.times do |index|
+	  Task.create!(location: Faker::Address.state,
+	                description: Faker::Lorem.paragraph(5),
+	                boss_id: Faker::Number.between(1, 10),
+	                tasker_id: Faker::Number.between(1, 10),
+									minutes: Faker::Number.between(1, 10) * 60,
+	                vehicle: Faker::Boolean.boolean(0.8),
+	                completed: Faker::Boolean.boolean(0.5),
+									price_rate: Faker::Number.between(30,100)
+	               )
+	  end
 
 # Boss.create(name: "john f", email: "john5@gmail.com", username: "john5", city: "Brooklyn")
 # Boss.create(name: "dave s", email: "dave55@gmail.com", username: "dave55", city: "Brooklyn")
@@ -22,30 +45,3 @@ Task.destroy_all
 # Tasker.create(name: "Rick s", email: "ricksan@gmail.com", username: "therick", city: "Manhattan", bio: "hi there how are you")
 # Tasker.create(name: "Walter", email: "walter6@gmail.com", username: "walter6", city: "Boston", bio: "hi there how are you")
 # Tasker.create(name: "Nathan", email: "nathan1@gmail.com", username: "nathan111", city: "Boston", bio: "hi there how are you")
-
-10.times do |index|
-  Boss.create!(name: Faker::Name.name,
-                email: Faker::Internet.email,
-                username: Faker::Internet.username(6),
-                city: Faker::Address.state,
-               )
-  end
-#
-	3.times do |index|
-	  Tasker.create!(name: Faker::Name.name,
-	                email: Faker::Internet.email,
-	                username: Faker::Internet.username(6),
-	                city: Faker::Address.state,
-	                bio: Faker::Lorem.paragraph(4),
-	                vehicle: Faker::Boolean.boolean(0.8)
-	               )
-	  end
-	3.times do |index|
-	  Task.create!(location: Faker::Address.state,
-	                description: Faker::Lorem.paragraph(5),
-	                boss_id: Faker::Number.between(4, 10) ,
-									minutes: Faker::Number.between(1, 10) * 60,
-	                vehicle: Faker::Boolean.boolean(0.8),
-									price_rate: Faker::Number.between(30,100),
-	               )
-	  end
