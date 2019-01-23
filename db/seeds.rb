69437b64ab8f0484765a22efbbfc3074884de3d9
Kudzanayi Dzvairo <kudzanayi@Kudzanayis-MacBook-Air.local>
2019-01-23 11:44:28 -0500

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,7 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Boss.destroy_all
-Tasker.destroy_all
+Task.destroy_all
 
 # Boss.create(name: "john f", email: "john5@gmail.com", username: "john5", city: "Brooklyn")
 # Boss.create(name: "dave s", email: "dave55@gmail.com", username: "dave55", city: "Brooklyn")
@@ -32,20 +31,20 @@ Tasker.destroy_all
                )
   end
 #
-# 	3.times do |index|
-# 	  Tasker.create!(name: Faker::Name.name,
-# 	                email: Faker::Internet.email,
-# 	                username: Faker::Internet.username(6),
-# 	                city: Faker::Address.state,
-# 	                bio: Faker::Lorem.paragraph(4),
-# 	                vehicle: Faker::Boolean.boolean(0.8)
-# 	               )
-# 	  end
+	3.times do |index|
+	  Tasker.create!(name: Faker::Name.name,
+	                email: Faker::Internet.email,
+	                username: Faker::Internet.username(6),
+	                city: Faker::Address.state,
+	                bio: Faker::Lorem.paragraph(4),
+	                vehicle: Faker::Boolean.boolean(0.8)
+	               )
+	  end
 	3.times do |index|
 	  Task.create!(location: Faker::Address.state,
 	                description: Faker::Lorem.paragraph(5),
-	                boss_id: Faker::Number.between(4, 10),
-									minutes: Faker::Number.between(1, 12),
+	                boss_id: Faker::Number.between(4, 10) ,
+									minutes: Faker::Number.between(1, 10) * 60,
 	                vehicle: Faker::Boolean.boolean(0.8),
 									price_rate: Faker::Number.between(30,100),
 	               )
